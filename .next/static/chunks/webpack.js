d793cdbfea43d3695880f5cc0808833ea9dabd1c
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-03-23 12:56:38 +0200

    correction of mobile height

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fa31eb6feec5723"; }
+/******/ 		__webpack_require__.h = function() { return "60e4d4280a6c9e52"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
