1407c495093fc996006057de81bb9d5e0834c2f3
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-05-17 11:48:40 +0300

    correction policy page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5385f2b3f33551b7"; }
+/******/ 		__webpack_require__.h = function() { return "c1bfec6cab571dad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
