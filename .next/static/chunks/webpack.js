166121adf253c40646401fcb89e459b9a9adf8f3
Evgheni <86593030+nev-front-dev@users.noreply.github.com>
2022-10-24 12:19:15 +0300

    planning images corrections

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "64dcb64f15e870c3"; }
+/******/ 		__webpack_require__.h = function() { return "680822f9d53078e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
